b10ce9423811b36484a828f786d14de8111900de
John Stockbauer <john@shippable.com>
2018-06-22 23:57:38 -0700

    successfully mocked players call

--- gamify.go
+++ gamify.go
@@ -6,23 +6,41 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/softsrv/gamify/steam"
 )
 
-//Index displays a basic welcome message
+//Index displays a basic welcome page
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
 }
 
-func Users(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	queryValues := r.URL.Query()
+// Players returns a list of steam players to the caller
+func Players(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	//queryValues := r.URL.Query()
+	var x mockapi.Service
+	pl, err := x.Players("fakeid")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprint(w, "failed to fetch players")
+		return
+	}
+	fmt.Println("here we go")
+	fmt.Fprintf(w, "%+v\n", pl)
+
 }
 
-func User(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
+// Friends accepts one steamID in the query and
+// returns all friends of that steamID
+func Friends(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	//queryValues := r.URL.Query()
 }
 
+// Games accepts one steamID in the query, and returns
+// all games owned by that steam user
 func Games(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	queryValues := r.URL.Query()
+	//queryValues := r.URL.Query()
+	// to construct url for game icon, use:
+	// http://media.steampowered.com/steamcommunity/public/images/apps/{appid}/{hash}.jpg
 }
 
 func main() {
@@ -30,8 +48,8 @@ func main() {
 
 	router := httprouter.New()
 	router.GET("/", Index)
-	router.GET("/users", Users)
-	router.GET("/users/:steamID", User)
+	router.GET("/players", Players)
+	router.GET("/friends", Friends)
 	router.GET("/games", Games)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
--- steam/mockapi.go
+++ steam/mockapi.go
@@ -1,8 +1,40 @@
 package mockapi
 
-type mockapi struct {
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Service struct {
 }
 
-func (m *mockapi) getUsers(steamID string) ([]Players, error) {
-	return
+// Player contains details about the Steam User
+type Player struct {
+	SteamID      string `json:"steamid"`
+	PersonaName  string `json:"personaname"`
+	AvatarSmall  string `json:"avatar"`
+	AvatarMedium string `json:"avatarmedium"`
+	AvatarFull   string `json:"avatarfull"`
+}
+
+type PlayerList struct {
+	Players []Player `json:"players"`
+}
+
+type PlayersResult struct {
+	Response PlayerList `json:"response"`
+}
+
+// Players accepts one or more steamIDs and returns a PlayersResult
+func (m *Service) Players(steamIDs string) (*PlayersResult, error) {
+	rawFriends, err := ioutil.ReadFile("steam/mockdata/players.json")
+	if err != nil {
+		return nil, err
+	}
+	var parsedFriends PlayersResult
+	parseErr := json.Unmarshal(rawFriends, &parsedFriends)
+	if parseErr != nil {
+		return nil, parseErr
+	}
+	return &parsedFriends, nil
 }
